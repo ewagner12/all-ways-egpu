43cd76dca8c4636f333a41135a5c55b8635e5de8
ewagner12 <50758619+ewagner12@users.noreply.github.com>
2024-12-14 19:05:18 -0500

    Correct nvidia_drm reload example
    Added nvidia_drm module parameters in entry point script example

--- all-ways-egpu-entry.sh
+++ all-ways-egpu-entry.sh
@@ -7,7 +7,7 @@ CML=$*
 ## For example Nvidia dGPU + Nvidia eGPU you may need to uncomment the following lines to manually reload the nvidia_drm module
 # for n in $(seq 1 30); do
 #     sleep 0.1
-#     modprobe -r nvidia_drm && modprobe nvidia_drm
+#     modprobe -r nvidia_drm && modprobe nvidia_drm modeset=1 fbdev=1
 #     if [ "$?" = 0 ]; then break; fi
 # done
 
